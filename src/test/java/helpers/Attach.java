2763f2eb51d3475e7f7f4b0cfde6193225621d16
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-04-04 08:38:26 +0300

    added URL getVideoUrl() example  "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -44,7 +44,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/bb421396d327e8fc0063fb43465832f7.mp4";
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
 
      //  System.out.println(sessionId());
         try {
